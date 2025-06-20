f37fa2dfd06aaacf02b5cf10d4fa684aed2708cd
pitos <pitos@KANAMI>
2025-06-20 17:34:23 +0200

    Dodaj pliki projektów.

---
+++ konwert/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace konwert
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.ComboBox comboCategory;
+        private System.Windows.Forms.ComboBox comboFrom;
+        private System.Windows.Forms.ComboBox comboTo;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblValue;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.comboCategory = new System.Windows.Forms.ComboBox();
+            this.comboFrom = new System.Windows.Forms.ComboBox();
+            this.comboTo = new System.Windows.Forms.ComboBox();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboCategory
+            // 
+            this.comboCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboCategory.FormattingEnabled = true;
+            this.comboCategory.Location = new System.Drawing.Point(120, 15);
+            this.comboCategory.Name = "comboCategory";
+            this.comboCategory.Size = new System.Drawing.Size(150, 23);
+            this.comboCategory.TabIndex = 0;
+            this.comboCategory.SelectedIndexChanged += new System.EventHandler(this.comboCategory_SelectedIndexChanged);
+            // 
+            // comboFrom
+            // 
+            this.comboFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboFrom.FormattingEnabled = true;
+            this.comboFrom.Location = new System.Drawing.Point(120, 55);
+            this.comboFrom.Name = "comboFrom";
+            this.comboFrom.Size = new System.Drawing.Size(150, 23);
+            this.comboFrom.TabIndex = 1;
+            // 
+            // comboTo
+            // 
+            this.comboTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboTo.FormattingEnabled = true;
+            this.comboTo.Location = new System.Drawing.Point(120, 95);
+            this.comboTo.Name = "comboTo";
+            this.comboTo.Size = new System.Drawing.Size(150, 23);
+            this.comboTo.TabIndex = 2;
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(120, 135);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(150, 23);
+            this.txtValue.TabIndex = 3;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(120, 175);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(150, 30);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Konwertuj";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.Location = new System.Drawing.Point(15, 220);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(255, 50);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.Location = new System.Drawing.Point(15, 15);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(100, 23);
+            this.lblCategory.TabIndex = 6;
+            this.lblCategory.Text = "Kategoria:";
+            this.lblCategory.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.Location = new System.Drawing.Point(15, 55);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(100, 23);
+            this.lblFrom.TabIndex = 7;
+            this.lblFrom.Text = "Z jednostki:";
+            this.lblFrom.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTo
+            // 
+            this.lblTo.Location = new System.Drawing.Point(15, 95);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(100, 23);
+            this.lblTo.TabIndex = 8;
+            this.lblTo.Text = "Na jednostkę:";
+            this.lblTo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblValue
+            // 
+            this.lblValue.Location = new System.Drawing.Point(15, 135);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(100, 23);
+            this.lblValue.TabIndex = 9;
+            this.lblValue.Text = "Wartość:";
+            this.lblValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(285, 285);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.comboTo);
+            this.Controls.Add(this.comboFrom);
+            this.Controls.Add(this.comboCategory);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Konwerter jednostek";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
+
---
+++ konwert/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+    namespace konwert
+    {
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+                InitializeCategories();
+            }
+
+            private void InitializeCategories()
+            {
+                comboCategory.Items.Add("Długość");
+                comboCategory.Items.Add("Waga");
+                comboCategory.Items.Add("Temperatura");
+                comboCategory.SelectedIndex = 0; // domyślnie długość
+            }
+
+            private void comboCategory_SelectedIndexChanged(object sender, EventArgs e)
+            {
+                comboFrom.Items.Clear();
+                comboTo.Items.Clear();
+
+                switch (comboCategory.SelectedItem.ToString())
+                {
+                    case "Długość":
+                        comboFrom.Items.AddRange(new object[] { "metry (m)", "kilometry (km)", "mile (mi)" });
+                        comboTo.Items.AddRange(new object[] { "metry (m)", "kilometry (km)", "mile (mi)" });
+                        break;
+                    case "Waga":
+                        comboFrom.Items.AddRange(new object[] { "kilogramy (kg)", "funty (lb)" });
+                        comboTo.Items.AddRange(new object[] { "kilogramy (kg)", "funty (lb)" });
+                        break;
+                    case "Temperatura":
+                        comboFrom.Items.AddRange(new object[] { "Celsjusz (°C)", "Fahrenheit (°F)" });
+                        comboTo.Items.AddRange(new object[] { "Celsjusz (°C)", "Fahrenheit (°F)" });
+                        break;
+                }
+
+                comboFrom.SelectedIndex = 0;
+                comboTo.SelectedIndex = 1;
+                lblResult.Text = "";
+            }
+
+            private void btnConvert_Click(object sender, EventArgs e)
+            {
+                double value;
+                if (!double.TryParse(txtValue.Text, out value))
+                {
+                    MessageBox.Show("Wprowadź poprawną liczbę.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string category = comboCategory.SelectedItem.ToString();
+                string fromUnit = comboFrom.SelectedItem.ToString();
+                string toUnit = comboTo.SelectedItem.ToString();
+
+                double result = double.NaN;
+
+                if (category == "Długość")
+                {
+                    double meters = ConvertLengthToMeters(value, fromUnit);
+                    result = ConvertMetersToLength(meters, toUnit);
+                }
+                else if (category == "Waga")
+                {
+                    double kg = ConvertWeightToKg(value, fromUnit);
+                    result = ConvertKgToWeight(kg, toUnit);
+                }
+                else if (category == "Temperatura")
+                {
+                    result = ConvertTemperature(value, fromUnit, toUnit);
+                }
+
+                if (double.IsNaN(result))
+                {
+                    lblResult.Text = "Niepoprawne jednostki.";
+                }
+                else
+                {
+                    lblResult.Text = string.Format("{0} {1} = {2:F4} {3}", value, fromUnit, result, toUnit);
+                }
+            }
+
+            private double ConvertLengthToMeters(double val, string unit)
+            {
+                if (unit.StartsWith("metry"))
+                    return val;
+                if (unit.StartsWith("kilometry"))
+                    return val * 1000;
+                if (unit.StartsWith("mile"))
+                    return val * 1609.34;
+                return double.NaN;
+            }
+
+            private double ConvertMetersToLength(double val, string unit)
+            {
+                if (unit.StartsWith("metry"))
+                    return val;
+                if (unit.StartsWith("kilometry"))
+                    return val / 1000;
+                if (unit.StartsWith("mile"))
+                    return val / 1609.34;
+                return double.NaN;
+            }
+
+            private double ConvertWeightToKg(double val, string unit)
+            {
+                if (unit.StartsWith("kilogramy"))
+                    return val;
+                if (unit.StartsWith("funty"))
+                    return val * 0.453592;
+                return double.NaN;
+            }
+
+            private double ConvertKgToWeight(double val, string unit)
+            {
+                if (unit.StartsWith("kilogramy"))
+                    return val;
+                if (unit.StartsWith("funty"))
+                    return val / 0.453592;
+                return double.NaN;
+            }
+
+            private double ConvertTemperature(double val, string from, string to)
+            {
+                if (from.StartsWith("Celsjusz") && to.StartsWith("Fahrenheit"))
+                    return val * 9 / 5 + 32;
+                if (from.StartsWith("Fahrenheit") && to.StartsWith("Celsjusz"))
+                    return (val - 32) * 5 / 9;
+                if (from == to)
+                    return val;
+
+                return double.NaN;
+            }
+        }
+    }
